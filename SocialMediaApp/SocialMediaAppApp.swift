0b076b1221e1cda49f89fd1bfc3ae17bfbc9f2a1
krozajac <krozajac@gmail.com>
2023-03-20 22:47:42 +0100

    Initial Commit

---
+++ SocialMediaApp/SocialMediaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaAppApp.swift
+//  SocialMediaApp
+//
+//  Created by Kerim Rozajac on 20. 3. 2023..
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
